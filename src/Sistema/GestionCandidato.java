8b72ae99ba336755dd04ebd6308924c0f0f2d097
DiegoChiu24 <diegochiu24@gmail.com>
2025-07-09 22:01:18 -0500

    Add files via upload

---
+++ src/Sistema/GestionCandidato.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Sistema;
+
+/**
+ *
+ * @author wachi
+ */
+public class GestionCandidato {
+
+    private Candidato[] listaCandidatos;
+    private int cantidadRegistrada;
+
+    public GestionCandidato(int capacidadMaxima) {
+        this.listaCandidatos = new Candidato[capacidadMaxima];
+        this.cantidadRegistrada = 0;
+    }
+
+    // Agregar un nuevo candidato
+    public boolean registrarCandidato(Candidato nuevoCandidato) {
+        if (cantidadRegistrada < listaCandidatos.length) {
+            listaCandidatos[cantidadRegistrada] = nuevoCandidato;
+            cantidadRegistrada++;
+            return true;
+        }
+        return false;
+    }
+
+    // Modificar candidato en un índice específico
+    public boolean modificarCandidato(int indice, Candidato candidatoActualizado) {
+        if (indice >= 0 && indice < cantidadRegistrada) {
+            listaCandidatos[indice] = candidatoActualizado;
+            return true;
+        }
+        return false;
+    }
+
+    // Eliminar candidato en un índice específico
+    public boolean eliminarCandidato(int indice) {
+        if (indice >= 0 && indice < cantidadRegistrada) {
+            for (int i = indice; i < cantidadRegistrada - 1; i++) {
+                listaCandidatos[i] = listaCandidatos[i + 1];
+            }
+            listaCandidatos[cantidadRegistrada - 1] = null;
+            cantidadRegistrada--;
+            return true;
+        }
+        return false;
+    }
+
+    // Obtener copia de candidatos registrados
+    public Candidato[] obtenerCandidatos() {
+        Candidato[] copia = new Candidato[cantidadRegistrada];
+        for (int i = 0; i < cantidadRegistrada; i++) {
+            copia[i] = listaCandidatos[i];
+        }
+        return copia;
+    }
+
+    public int obtenerCantidadRegistrada() {
+        return cantidadRegistrada;
+    }
+}
